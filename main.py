8d33ef22d261f19cb8714b9130cef828caf059b8
IgorKrol <igork08@gmail.com>
2021-12-07 23:59:43 +0200

    Maze builder working Fixed colors (maze light-grey) Maze Builder animated Start-End points marked (with red color)

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import cv2.cv2 as cv
 import numpy as np
 import time
 import sys
+
 sys.setrecursionlimit(5000)
 
+BACKGROUND_HEIGHT, BACKGROUND_WIDTH = 780, 1200
+NODES_IN_ROW = int(BACKGROUND_WIDTH / 20)
+NODES_IN_COL = int(BACKGROUND_HEIGHT / 20)
 
-BACKGROUND_HEIGHT, BACKGROUND_WIDTH = 780,1200
-NODES_IN_ROW = int (BACKGROUND_WIDTH / 20)
-NODES_IN_COL = int (BACKGROUND_HEIGHT / 20)
 
 # represent nodes in divided rectangle
 class Node:
@@ -18,8 +19,9 @@ class Node:
         self.pos = position
         self.neighbors = []
         self.visited = False
+        self.endNode = False
 
-    # checker if node has single neighbor (parent)
+    # checker if node has single neighbor (parent) - NOT NEEDED
     def is_leaf(self):
         if len(self.neighbors) == 1:
             return True
@@ -32,20 +34,30 @@ class Node:
         other.neighbors.append(self)
 
     # draw connection between 2 nodes
-    def draw_connection(self, mat, other):
-
-        mat = cv.rectangle(mat, (self.pos[0] - 5, self.pos[1] - 5),
-                     (other.pos[0] + 5, other.pos[1] + 5),
-                     (255, 255, 255), -1)
-        mat = cv.rectangle(mat, (self.pos[0] - 5, self.pos[1] - 5),
-                                 (self.pos[0] + 5, self.pos[1] + 5),
-                                 (255, 0, 0), -1)
-        mat = cv.rectangle(mat, (other.pos[0] - 5, other.pos[1] - 5),
-                              (other.pos[0] + 5, other.pos[1] + 5),
-                              (0, 255, 0), -1)
+    def draw_connection(self, mat, other, color=(155, 155, 155)):
+        mat = cv.rectangle(mat, (self.pos[0] - 4, self.pos[1] - 4),
+                           (other.pos[0] + 4, other.pos[1] + 4),
+                           color, -1)
+        mat = cv.rectangle(mat, (self.pos[0] - 4, self.pos[1] - 4),
+                           (self.pos[0] + 4, self.pos[1] + 4),
+                           color, -1)
+        mat = cv.rectangle(mat, (other.pos[0] - 4, other.pos[1] - 4),
+                           (other.pos[0] + 4, other.pos[1] + 4),
+                           color, -1)
 
         return mat
 
+
+class DrawAnimation:
+    def __init__(self, startNode: Node, endNode: Node):
+        self.endNode = endNode
+        self.startNode = startNode
+
+    def draw(self, window_name, mat):
+        self.startNode.draw_connection(mat, self.startNode, (0, 0, 255))
+        self.endNode.draw_connection(mat, self.endNode, (0, 0, 255))
+        cv.imshow(window_name, mat)
+        return
 
 
 # init rectangle graph with connected neighbors
@@ -53,11 +65,11 @@ def create_graph():
     # creating rectangle of nodes with 10 pixels in between
     nodes = [[Node((i * 20 + 5, j * 20 + 5)) for i in range(NODES_IN_ROW)] for j in range(NODES_IN_COL)]
     # connect neighboring nodes
-    for i in range(1,len(nodes)):
-        for j in range(0,len(nodes[0])):
+    for i in range(1, len(nodes)):
+        for j in range(0, len(nodes[0])):
             nodes[i][j].connect_nodes(nodes[i - 1][j])
     for i in range(0, len(nodes)):
-         for j in range(1, len(nodes[0])):
+        for j in range(1, len(nodes[0])):
             nodes[i][j].connect_nodes(nodes[i][j - 1])
 
     # return back list with neighbors connected
@@ -65,51 +77,43 @@ def create_graph():
 
 
 # draw maze with random dfs
-def draw_maze(mat, root):
-    if (not root.is_leaf()):
-        root.visited = True
-        neighborsCircus = np.random.randint(len(root.neighbors),size=len(root.neighbors))
-        print(len(root.neighbors))
+def draw_maze(mat, startNode, endNode):
+    if (not startNode.is_leaf()):
+        startNode.visited = True
+        neighborsCircus = np.random.randint(len(startNode.neighbors), size=len(startNode.neighbors))
+        da = DrawAnimation(startNode, endNode)
         random.shuffle(neighborsCircus)
         for n in neighborsCircus:
-            mat = make_path(mat, root, root.neighbors[n])
+            mat = make_path(mat, da, startNode, startNode.neighbors[n])
     return mat
 
-def make_path(mat, prevNode, presentNode):
-    if presentNode.visited != True :
+
+def make_path(mat, da: DrawAnimation, prevNode: Node, presentNode: Node):
+    if presentNode.visited != True:
         presentNode.visited = True
         mat = presentNode.draw_connection(mat, prevNode)
-        # time.sleep(0.1)
-        # if(not presentNode.is_leaf()):
-        neighborsCircus = np.random.randint(len(presentNode.neighbors),size=len(presentNode.neighbors))
-        random.shuffle(neighborsCircus)
-        for n in neighborsCircus:
-            mat = make_path(mat, presentNode, presentNode.neighbors[n])
+        cv.waitKey(1)
+        da.draw("main_window", mat)
+        if (not presentNode.endNode):
+            neighborsCircus = np.random.randint(len(presentNode.neighbors), size=len(presentNode.neighbors))
+            random.shuffle(neighborsCircus)
+            for n in neighborsCircus:
+                mat = make_path(mat, da, presentNode, presentNode.neighbors[n])
     return mat
+
 
 # init all needed variables and builds the maze
 def build_maze():
     # black blank image
-    blank_image = np.zeros(shape=[BACKGROUND_HEIGHT, BACKGROUND_WIDTH, 3], dtype=np.uint8)
-    maze = blank_image  #background black screen
-    nodes = create_graph()
-    maze = draw_maze(maze, nodes[0][0])
-    # maze = nodes[1][1].draw_connection(maze, nodes[2][1])
-    # maze = nodes[1][1].draw_connection(maze, nodes[1][2])
-    # maze = cv.rectangle(maze, nodes[0][0].pos, nodes[0][1].pos, (255, 255, 255), -1)
+    blank_image = np.full(shape=[BACKGROUND_HEIGHT, BACKGROUND_WIDTH, 3], fill_value=[0, 0, 0], dtype=np.uint8)
+    maze = blank_image  # background black screen
+    nodes = create_graph()  # building graph based on matrix rectangle
 
+    finishNode, startNode = nodes[len(nodes) - 1][len(nodes[0]) - 1], nodes[0][0]
+    finishNode.endNode = True
+    maze = draw_maze(maze, startNode, finishNode)
 
-    # for row in nodes:
-    #     for n in row:
-    #         for neighbor in n.neighbors:
-    #             maze = n.draw_connection(maze, neighbor)
-    #             cv.imshow("Black Blank", maze)
-    #             time.sleep(0.1)
-
-    # print(blank_image.shape)
-    # cv.imshow("Black Blank", blank_image)
-    cv.imshow("Black Blank", maze)
-    # white blank image
+    # cv.imshow("Black Blank", maze)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
